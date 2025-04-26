773c182d094a0d697843c92b19dda8368bd6885a
A-lex-Ra <rastorguev2047@gmail.com>
2025-04-26 18:00:26 +0300

    add openapi-spec.json generation

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
 
   app.useGlobalPipes(new ValidationPipe());
 
@@ -17,7 +23,8 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
+  writeFileSync('./openapi-spec.json', JSON.stringify(document, null, 2));
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
